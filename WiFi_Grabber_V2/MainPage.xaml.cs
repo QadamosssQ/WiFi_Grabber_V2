750e3d847f20ea409c90a0bddbc4165a4d41ca6c
QadamosssQ <altadamoss@gmail.com>
2023-09-13 21:18:32 +0200

    Finally add working listview

--- WiFi_Grabber_V2/MainPage.xaml.cs
+++ WiFi_Grabber_V2/MainPage.xaml.cs
@@ -1,10 +1,42 @@
-﻿namespace WiFi_Grabber_V2;
+﻿using System.Collections.ObjectModel;
 
-public partial class MainPage : ContentPage
+namespace WiFi_Grabber_V2
 {
-    public MainPage()
+    public partial class MainPage : ContentPage
     {
-        InitializeComponent();
-    }
+        public class Fruit
+        {
+            public string FruitName { get; set; }
+        }
 
+        ObservableCollection<Fruit> fruits = new ObservableCollection<Fruit>();
+        public ObservableCollection<Fruit> Fruits { get { return fruits; } }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            fruits.Add(new Fruit() { FruitName = "Apple" });
+            fruits.Add(new Fruit() { FruitName = "Orange" });
+            fruits.Add(new Fruit() { FruitName = "Banana" });
+            fruits.Add(new Fruit() { FruitName = "Grape" });
+            fruits.Add(new Fruit() { FruitName = "Mango" });
+
+            FruitListView.ItemsSource = fruits;
+
+            // Dodanie obsługi zdarzenia ItemTapped
+            FruitListView.ItemTapped += OnFruitListViewItemTapped;
+        }
+
+        private async void OnFruitListViewItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item is Fruit selectedFruit)
+            {
+                await DisplayAlert("Selected Fruit", selectedFruit.FruitName, "OK");
+            }
+
+            // Oznaczenie elementu jako niezaznaczonego, aby umożliwić ponowne kliknięcie
+            FruitListView.SelectedItem = null;
+        }
+    }
 }

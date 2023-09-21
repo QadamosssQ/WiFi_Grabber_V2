353c4854982745d7705c64ffb2602e2307ba6d21
QadamosssQ <altadamoss@gmail.com>
2023-09-21 20:50:50 +0200

    update

---
+++ WiFi_Grabber_V2/Classes/Network.cs
@@ -0,0 +1,6 @@
+﻿namespace WiFi_Grabber_V2.Classes;
+
+public class Network
+{
+    public string NetworkName { get; set; } 
+}
--- WiFi_Grabber_V2/MainPage.xaml.cs
+++ WiFi_Grabber_V2/MainPage.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Text;
+using NativeWifi;
+
+using System.Collections.ObjectModel;
+
 
 namespace WiFi_Grabber_V2
 {
@@ -11,21 +16,31 @@ namespace WiFi_Grabber_V2
 
         ObservableCollection<Fruit> fruits = new ObservableCollection<Fruit>();
         public ObservableCollection<Fruit> Fruits { get { return fruits; } }
+        
+        static string GetStringForSSID(Wlan.Dot11Ssid ssid) =>
+            Encoding.ASCII.GetString(ssid.SSID, 0, (int)ssid.SSIDLength);
 
         public MainPage()
         {
             InitializeComponent();
+            
+            var client = new WlanClient();
+            foreach (var wlanIface in client.Interfaces)
+            {
+                var networks = wlanIface.GetAvailableNetworkList(0);
+                
+                foreach (var profileInfo in wlanIface.GetProfiles())
+                {
+                    string name = profileInfo.profileName;
+                    string xml = wlanIface.GetProfileXml(profileInfo.profileName);
+                    fruits.Add(new Fruit() { FruitName = name });
+                }
+            }
 
-            fruits.Add(new Fruit() { FruitName = "Apple" });
-            fruits.Add(new Fruit() { FruitName = "Orange" });
-            fruits.Add(new Fruit() { FruitName = "Banana" });
-            fruits.Add(new Fruit() { FruitName = "Grape" });
-            fruits.Add(new Fruit() { FruitName = "Mango" });
-
-            FruitListView.ItemsSource = fruits;
+            NetworkListView.ItemsSource = fruits;
 
             // Dodanie obsługi zdarzenia ItemTapped
-            FruitListView.ItemTapped += OnFruitListViewItemTapped;
+            NetworkListView.ItemTapped += OnFruitListViewItemTapped;
         }
 
         private async void OnFruitListViewItemTapped(object sender, ItemTappedEventArgs e)
@@ -36,7 +51,7 @@ namespace WiFi_Grabber_V2
             }
 
             // Oznaczenie elementu jako niezaznaczonego, aby umożliwić ponowne kliknięcie
-            FruitListView.SelectedItem = null;
+            NetworkListView.SelectedItem = null;
         }
     }
 }
